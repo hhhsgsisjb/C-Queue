5ae12b6cc13451ef27ed65998718860775b587d8
FREEMAN <FREEMAN@DESKTOP-RODT8RQ>
2024-11-13 19:48:41 +0300

    Add project files.

---
+++ ConsoleApp44/Class1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp44
+{
+    class CQueue
+    {
+        int front;
+        int rear;
+        int size;
+        int[] CQueuearr;
+        public CQueue(int len)
+        {
+            size = len;
+            CQueuearr = new int[size];
+            front = -1;
+            rear = -1;
+        }
+        public bool IsEmpty()
+        {
+            return (rear == -1);
+        }
+        public bool IsFull()
+        {
+            return ((front == 0 && rear == size - 1) || (front == rear + 1));
+        }
+        //Add element
+        public void Enqueue(int element)
+        {
+            rear = (rear + 1) % size;
+            CQueuearr[rear] = element;
+            if (front == -1) // for first item added
+                front++;
+        }
+        //Delete element
+        public int Dequeue()
+        {
+            int x = CQueuearr[front];
+            if (front == rear) //for last element removed
+            {
+                front = -1;
+                rear = -1;
+            }
+            else
+                front = (front + 1) % size;
+            return x;
+        }
+        public int QueueSize()
+        {
+            return ((size - front + rear) % size) + 1;
+        }
+        //Get First element
+        public int First()
+        {
+            return CQueuearr[front];
+        }
+        //Get Last element
+        public int Last()
+        {
+            return CQueuearr[rear];
+        }
+        public void Display()
+        {
+            Console.WriteLine("The elements of CQueue is ");
+            int i;
+            // display CQ elements
+            for (i = front; i != rear; i = (i + 1) % size)
+            {
+                Console.WriteLine(CQueuearr[i]);
+            }
+            // display last element only
+            Console.WriteLine(CQueuearr[i]);
+        }
+    }
+}
---
+++ ConsoleApp44/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp44
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("input the size of CQueue");
+            int len = int.Parse(Console.ReadLine());
+            CQueue CQ = new CQueue(len);
+            // Add multiple elements to CQueue
+            for (int i = 0; i < 10; i++)
+            {
+                if (CQ.IsFull())
+                {
+                    Console.WriteLine("CQueue is Full!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("input your element");
+                    int x = int.Parse(Console.ReadLine());
+                    CQ.Enqueue(x);
+                }
+            }
+            // Delete two element from CQueue
+            for (int i = 0; i < 2; i++)
+            {
+                if (CQ.IsEmpty())
+                {
+                    Console.WriteLine("CQueue is Empty!");
+                    break;
+                }
+                else
+                {
+                    int x = CQ.Dequeue();
+                    Console.WriteLine("The Deleted element is " + x);
+                }
+            }
+            // CQueue Size
+            if (CQ.IsEmpty())
+                Console.WriteLine("CQueue is Empty!");
+            else
+                Console.WriteLine("The CQueue size is " + CQ.QueueSize());
+           
+            // get First element
+            if (CQ.IsEmpty())
+                Console.WriteLine("CQueue is Empty!");
+            else
+                Console.WriteLine("The First element is " + CQ.First());
+            // get Last element
+            if (CQ.IsEmpty())
+                Console.WriteLine("CQueue is Empty!");
+            else
+                Console.WriteLine("The Last element is " + CQ.Last());
+            //Display CQueue elements
+            if (CQ.IsEmpty())
+                Console.WriteLine("CQueue is Empty!");
+            else
+                CQ.Display();
+            // Add elements to CQueue
+            for (int i = 0; i < 10; i++)
+            {
+                if (CQ.IsFull())
+                {
+                    Console.WriteLine("CQueue is Full!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("input your element");
+                    int x = int.Parse(Console.ReadLine());
+                    CQ.Enqueue(x);
+                }
+            }
+            // CQueue Size
+            if (CQ.IsEmpty())
+                Console.WriteLine("CQueue is Empty!");
+            else
+                Console.WriteLine("The CQueue size is " + CQ.QueueSize());
+            //Display CQueue elements
+            if (CQ.IsEmpty())
+                Console.WriteLine("CQueue is Empty!");
+            else
+                CQ.Display();
+
+            // Delete multiple element from CQueue
+            for (int i = 0; i < 10; i++)
+            {
+                if (CQ.IsEmpty())
+                {
+                    Console.WriteLine("CQueue is Empty!");
+                    break;
+                }
+                else
+                {
+                    int x = CQ.Dequeue();
+                    Console.WriteLine("The Deleted element is " + x);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
